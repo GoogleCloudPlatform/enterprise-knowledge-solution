ab2d8cdd4d3286a2190faf3c8675f6ba2e5bdde3
anuradha-bajpai-google <78390589+anuradha-bajpai-google@users.noreply.github.com>
2024-08-08 11:50:10 -0700

    DocAI Form Parser microservice (#12)
    * DocAI form parser processor integration
    
    * form processor build conatiner image script
    
    * DocAI form parser code integration
    
    * DocAI Form Parser fixes
    
    * Changes:
     - Re-sync'd the development constraints (shared) based on the form
       parser requirements.in
     - Moved requirements.txt to requirements.in for form parser
     - Updated tasks.py to also generate requirements.txt from
       requirements.in
     - Reformatted terraform from pre-commit
    
    ---------
    
    Co-authored-by: Mark Scannell <scannell@google.com>

--- sample-deployments/composer-orchestrated-process/main.tf
+++ sample-deployments/composer-orchestrated-process/main.tf
@@ -82,6 +82,15 @@ module "processor" {
   cloud_run_job_name = local.cloud_run_job_name
 }
 
+module "form_parser_processor" {
+  source            = "../../components/processing/form_parser/deployment"
+  project_id        = var.project_id
+  region            = var.region
+  location          = var.vertex_ai_data_store_region
+  gcs_input_prefix  = module.common_infra.gcs_process_bucket_name
+  gcs_output_prefix = module.common_infra.gcs_process_bucket_name
+}
+
 module "dpu_workflow" {
   source           = "../../components/dpu-workflow/terraform"
   region           = var.region
