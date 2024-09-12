47e6a9edac12652568fae61e90ada4aee97d4345
Eyal Ben Ivri <eyalbenivri@google.com>
2024-09-12 18:31:28 +0200

    small fix to the trigger_workflow.sh to match configurations

--- sample-deployments/composer-orchestrated-process/scripts/trigger_workflow.sh
+++ sample-deployments/composer-orchestrated-process/scripts/trigger_workflow.sh
@@ -17,7 +17,7 @@
 # Just a small helper to your developers - a small bash function to trigger the DAG from the command line:
 
 function trigger_dag() {
-  
+
   json_config=$(cat <<EOF
   {
     "input_bucket": "docs-input-${PROJECT_ID}",
@@ -57,9 +57,11 @@ function trigger_dag() {
             "processor": "dpu-doc-processor"
         }
     ],
-    "pdf_classifier_project_id": "${PROJECT_NUM}",
-    "pdf_classifier_location": "${DOC_AI_REGION}",
-    "pdf_classifier_processor_id": "${DOC_AI_PROCESSOR_ID}"
+    "classifier": {
+        "location": "${DOC_AI_REGION}",
+        "processor_id": "${DOC_AI_PROCESSOR_ID}",
+        "project_id": "${DOC_AI_PROJECT_ID}"
+    }
 }
 EOF
 )
@@ -73,15 +75,25 @@ set -o nounset
 . scripts/common.sh
 
 # Check if PROJECT_ID is set, otherwise prompt for input
-if [[ -z "$PROJECT_ID" ]]; then
-  read -p "Enter PROJECT_ID: " PROJECT_ID 
+if [[ -z "${PROJECT_ID:-}" ]]; then
+  read -p "Enter PROJECT_ID: " PROJECT_ID
 else
   echo "PROJECT_ID is set to: $PROJECT_ID"
 fi
 [[ -z "$PROJECT_ID" ]] && echo "PROJECT_ID is required." && exit 1
 
+
+# Check if DOC_AI_PROJECT_ID is set, otherwise prompt for input
+if [[ -z "${DOC_AI_PROJECT_ID:-}" ]]; then
+  read -p "Enter DOC_AI_PROJECT_ID: " DOC_AI_PROJECT_ID
+else
+  echo "DOC_AI_PROJECT_ID is set to: $DOC_AI_PROJECT_ID"
+fi
+[[ -z "$DOC_AI_PROJECT_ID" ]] && echo "DOC_AI_PROJECT_ID is required." && exit 1
+
+
 # Check if DOC_AI_REGION is set, otherwise prompt for input
-if [[ -z "$DOC_AI_REGION" ]]; then
+if [[ -z "${DOC_AI_REGION:-}" ]]; then
   read -p "Enter DOC_AI_REGION: " DOC_AI_REGION
 else
   echo "DOC_AI_REGION is set to: $DOC_AI_REGION"
@@ -89,7 +101,7 @@ fi
 [[ -z "$DOC_AI_REGION" ]] && echo "DOC_AI_REGION is required." && exit 1
 
 # Check if Composer Location is set, otherwise prompt for input
-if [[ -z "$COMPOSER_LOCATION" ]]; then
+if [[ -z "${COMPOSER_LOCATION:-}" ]]; then
   read -p "Enter Composer Location: " COMPOSER_LOCATION
 else
   echo "Composer Location is set to: $COMPOSER_LOCATION"
@@ -98,14 +110,14 @@ fi
 
 
 # Check if DOC_AI_PROCESSOR_ID is set, otherwise prompt for input
-if [[ -z "$DOC_AI_PROCESSOR_ID" ]]; then
+if [[ -z "${DOC_AI_PROCESSOR_ID:-}" ]]; then
   read -p "Enter DOC_AI_PROCESSOR_ID: " DOC_AI_PROCESSOR_ID
 else
   echo "DOC_AI_PROCESSOR_ID is set to: $DOC_AI_PROCESSOR_ID"
 fi
 [[ -z "$DOC_AI_PROCESSOR_ID" ]] && echo "DOC_AI_PROCESSOR_ID is required." && exit 1
 
-PROJECT_NUM=$(gcloud projects list | grep ${PROJECT_ID} | awk '{print $3}')
+
 
 section_open "Trigger DAG"
     trigger_dag
