c58c8dbe7054c28b4830347250975cf5ca73f43e
Eyal Ben Ivri <eyalbenivri@gmail.com>
2024-09-19 17:50:47 +0200

    Cleaned up config managment so that file supported is configured once in the DAG, and passed to the processing job (#73)
    * - In the Processing CRJ, supported files is now given as an argument and are mapped to specific operations in the processing-msg job
    - Argument is given as a CLI argument, and is required
    - Special argparse.Action was implemented to parse key:value pairs into a dict
    - DAG was adapted to pass in argument to the CRJ
    
    Important Note: This means the permitted values in the DAG config are the same structure, but support different values, that are actually being passed ot the CRJ.
    
    * adapted trigger_workflow.sh script to match new configuration values
    
    * added missing new parameter to call
    
    ---------
    
    Co-authored-by: Eyal Ben Ivri <eyalbenivri@google.com>

--- components/processing/libs/processor-msg/src/processors/msg/run.py
+++ components/processing/libs/processor-msg/src/processors/msg/run.py
@@ -17,7 +17,25 @@ import argparse
 import logging
 
 from processors.base.gcsio import GCSPath
-from processors.msg.main_processor import process_all_objects
+from processors.msg.main_processor import process_all_objects, Processors
+
+# Specialized action to parse multiple key-value pairs into a dict
+class keyvalue(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string = None, 
+                 *args, **kwargs):
+        # if this is the first time we call this, we need to make sure that 
+        # the namespace contains a dict for our `dest`
+        if (not hasattr(namespace, self.dest) or not 
+            getattr(namespace, self.dest)):
+            setattr(namespace, self.dest, dict())
+        
+        # for each key-value pair, parse the key-value (seperated by an equal
+        # sign), validate the key (starts with a dot) and store in the dict
+        for value in values:
+            key, value = list(map(lambda x: x.strip(), value.split(":")))
+            if not key.startswith("."):
+                key = "." + key
+            getattr(namespace, self.dest)[key] = value
 
 
 def main() -> None:
@@ -42,6 +60,17 @@ def main() -> None:
         default="",
         help="BigQuery fully qualified table to write results",
     )
+    all_processors = ", ".join([x.value for x in Processors])
+    parser.add_argument("--file-type", 
+                        metavar="KEY:VALUE",
+                        dest="supported_files",
+                        help="Set all supported file types in the form of "
+                             "FILE_TYPE:PROCESSOR. Minimum one is required. "
+                             "Multiple entries are possible. Supported "
+                             f"processors are {all_processors}",
+                        nargs="+",
+                        required=True,
+                        action=keyvalue)
 
     args = parser.parse_args()
 
@@ -51,6 +80,7 @@ def main() -> None:
     process_all_objects(
         GCSPath(args.process_dir),
         GCSPath(args.reject_dir),
+        args.supported_files,    
         write_json=args.write_json,
         write_bigquery=args.write_bigquery,
     )
