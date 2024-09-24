aa2922e9fde5567a2c5f787eea06fa1dce856d4e
Charlie Wang <charlie.zwang@gmail.com>
2024-09-24 15:53:52 +0200

    review comments

--- components/webui/terraform/build.tf
+++ components/webui/terraform/build.tf
@@ -33,7 +33,13 @@ module "app_build" {
   source = "github.com/terraform-google-modules/terraform-google-gcloud?ref=db25ab9c0e9f2034e45b0034f8edb473dde3e4ff" # commit hash of version 3.5.0
 
   create_cmd_entrypoint = "gcloud"
-  create_cmd_body       = "builds submit --region ${var.region} --project ${var.project_id} --config \"${local_file.cloudbuild_config.filename}\" \"${path.module}/../../..\""
+  create_cmd_body       = <<-EOT
+    builds submit \
+      --project ${var.project_id} \
+      --region ${var.region} \
+      --config ${local_file.cloudbuild_config.filename} \
+      "${path.module}/../../.."
+  EOT
   enabled               = true
 
   create_cmd_triggers = {
