86bf2d4057634caa4423d5aa7f6182582d2a00f6
Elliot <ellioteaton@gmail.com>
2024-10-16 15:53:03 +0000

    cleanup common.sh.
    1: "yes | gcloud auth" has some different behavior across Cloud Shell vs local terminal, so it's safer to prompt the user and get a consistent result.
    2: update logic to not change ADC if a service account is already in use, but give a warning to set ADC separately

--- sample-deployments/composer-orchestrated-process/scripts/common.sh
+++ sample-deployments/composer-orchestrated-process/scripts/common.sh
@@ -207,8 +207,9 @@ enable_builder_roles() {
 set_adc() {
   # check if the script is manually triggered by a user, or automated in CI build by a service account. Different methods of ADC for each
   if echo "$ACTIVE_PRINCIPAL" | grep "iam.gserviceaccount.com"; then
-    gcloud config set auth/impersonate_service_account "${SERVICE_ACCOUNT_ID}"
+    echo "WARNING: Setting application default credentials with an impersonated service account requires an interactive sign-in flow with your username and password, but this script has been run with a service account identity. Ensure that you set ADC with the correct service account before running terraform commands. https://cloud.google.com/docs/authentication/provide-credentials-adc"
+
   else
-    yes | gcloud auth application-default login --impersonate-service-account="${SERVICE_ACCOUNT_ID}"
+    gcloud auth application-default login --impersonate-service-account="${SERVICE_ACCOUNT_ID}"
   fi
 }
