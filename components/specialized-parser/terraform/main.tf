1ae01e276d9eff0b0af89df3a267d76285cd9eda
Eyal Ben Ivri <eyalbenivri@google.com>
2024-11-15 15:06:03 +0100

    More Linting

--- components/specialized-parser/terraform/main.tf
+++ components/specialized-parser/terraform/main.tf
@@ -107,6 +107,11 @@ resource "google_compute_subnetwork" "cloud-run-subnet" {
   region                   = var.region
   network                  = var.network
   private_ip_google_access = true
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
 }
 
 
