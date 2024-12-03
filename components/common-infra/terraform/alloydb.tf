2eeec731e5d4b0e63e1012b592d089bb8362cc5a
Elliot <ellioteaton@gmail.com>
2024-12-03 11:48:24 +0000

    clean up definition of subnet ranges. 1: psa was getting randomly allocated, which sometimes led to a conflict with the composer subnet. 2: serverless access connector was defining a subnet, but then explicitly creating its own range instead of using that subnet. Now simplified to use the subnet created for this purpose.

--- components/common-infra/terraform/alloydb.tf
+++ components/common-infra/terraform/alloydb.tf
@@ -13,11 +13,12 @@
 # limitations under the License.
 
 resource "google_vpc_access_connector" "vpc_connector" {
-  project       = module.project_services.project_id
-  name          = "alloy-db-vpc-connector"
-  region        = var.region
-  network       = local.vpc_network_id
-  ip_cidr_range = "10.8.0.0/28"
+  project = module.project_services.project_id
+  name    = "alloy-db-vpc-connector"
+  region  = var.region
+  subnet {
+    name = google_compute_subnetwork.serverless_connector_subnet.name
+  }
   min_instances = 2
   max_instances = 3
 }
@@ -39,8 +40,9 @@ resource "google_compute_global_address" "private_ip_address" {
   name          = "private-ip-address"
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
-  prefix_length = 16
+  prefix_length = 24
   network       = local.vpc_network_id
+  address       = var.psa_reserved_address
 }
 
 resource "google_service_networking_connection" "default" {
