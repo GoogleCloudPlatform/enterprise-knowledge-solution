207943f9a71e2a1cdafbde3adc1deeb08870d288
eeaton <ellioteaton@gmail.com>
2024-12-13 13:18:23 +0000

    chore: merge updates from fork (#177)
    Co-authored-by: eeaton <eeaton@users.noreply.github.com>

--- build/test/functional_test.go
+++ build/test/functional_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,45 +18,46 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"os/exec"
+	"regexp"
 	"testing"
 
-	logging "cloud.google.com/go/logging/logadmin"
-	envconfig "github.com/sethvargo/go-envconfig"
-	assert "github.com/stretchr/testify/assert"
+	"github.com/sethvargo/go-envconfig"
+	"github.com/stretchr/testify/assert"
 )
 
-type LoggingConfig struct {
-	PROJECT_ID string `env:"PROJECT_ID"`
+type CLIConfig struct {
+	PROJECT_ID        string `env:"PROJECT_ID"`
+	LOCATION          string `env:"LOCATION"`
+	COMPOSER_ENV_NAME string `env:"COMPOSER_ENV_NAME"`
+	DAG_ID            string `env:"DAG_ID"`
 }
 
-var filters []string = []string{
-	`resource.labels.container_name="airflow-scheduler" AND textPayload:("initial_load_from_input_bucket.list_all_input_files" AND "state=success")`,
-	`resource.labels.container_name="airflow-scheduler" AND textPayload:("initial_load_from_input_bucket.has_files_to_process" AND "state=success")`,
-}
-
-func TestLogExists(t *testing.T) {
+func TestDAGState(t *testing.T) {
 	ctx := context.Background()
-	var c LoggingConfig
+	var c CLIConfig
 	if err := envconfig.Process(ctx, &c); err != nil {
 		log.Fatal(err)
 	}
-	client, err := logging.NewClient(ctx, c.PROJECT_ID)
+
+	cmd := exec.Command(
+		"gcloud", "composer", "environments", "run", c.COMPOSER_ENV_NAME,
+		"--location", c.LOCATION,
+		"dags", "list-runs",
+		"--", "-d", c.DAG_ID,
+	)
+
+	out, err := cmd.Output()
 	if err != nil {
-		t.Fatalf("Failed to create client: %v", err)
+		t.Fatalf("Failed to execute gcloud command: %v", err)
 	}
-	defer client.Close()
 
-	for _, filter := range filters {
-		t.Run(filter, func(t *testing.T) {
-			fmt.Printf("Checking filter: %s\n", filter)
-
-			it := client.Entries(ctx, logging.Filter(filter))
-			entry, err := it.Next()
-
-			assert.NoError(t, err, "it.Next() should not return an error")
-			assert.NotNil(t, entry, "Log entry should not be nil")
-		})
+	output := string(out)
+	matched, err := regexp.MatchString(`| success |`, output)
+	if err != nil {
+		t.Fatalf("Failed to match regex: %v", err)
 	}
+
+	assert.True(t, matched, "DAG run with state 'success' not found in gcloud output")
 }
