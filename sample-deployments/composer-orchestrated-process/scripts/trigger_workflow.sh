0f2208c7b4e2c34a286603379eb555e0772238ae
Elliot <ellioteaton@gmail.com>
2025-01-16 11:12:37 +0000

    try loosening `-o errexit` setting in script, this might cause the golang code to exit instead of catching the error

--- sample-deployments/composer-orchestrated-process/scripts/trigger_workflow.sh
+++ sample-deployments/composer-orchestrated-process/scripts/trigger_workflow.sh
@@ -70,7 +70,7 @@ EOF
   gcloud composer environments run dpu-composer --project="$PROJECT_ID" --location "$(echo "$outputs" | jq -r ".composer_location.value")" dags trigger -- -c "${json_config}" run_docs_processing
 }
 
-set -o errexit
+#set -o errexit
 set -o nounset
 set -x
 
