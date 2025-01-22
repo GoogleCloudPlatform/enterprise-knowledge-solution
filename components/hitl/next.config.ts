bb786e24f1a1a3c2ed5fe5270062218129700441
Eyal Ben Ivri <eyalbenivri@google.com>
2025-01-22 09:49:36 +0100

    intermediate checkin

--- components/hitl/next.config.ts
+++ components/hitl/next.config.ts
@@ -18,6 +18,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: "standalone",
 };
 
 export default nextConfig;
---
+++ components/hitl/terraform/main.tf
@@ -0,0 +1,28 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+/*
+ * Activate required service API:s
+ */
+module "project_services" {
+  source                      = "github.com/terraform-google-modules/terraform-google-project-factory.git//modules/project_services?ref=ff00ab5032e7f520eb3961f133966c6ced4fd5ee" # commit hash of version 17.0.0
+  project_id                  = var.project_id
+  disable_services_on_destroy = false
+  disable_dependent_services  = false
+  activate_apis = [
+    "compute.googleapis.com",
+    "iap.googleapis.com",
+    "aiplatform.googleapis.com"
+  ]
+}
